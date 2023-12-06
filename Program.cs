8e308c5a4a909a05856991bf504b7c9fefde74ef
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-06 21:54:54 +0700

    hien_thi_loi_chao

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("nhap ten cua ban : ");
+        string tencuaban = Console.ReadLine();
+        Console.WriteLine("Xin chao : "+tencuaban);
+}
+}
